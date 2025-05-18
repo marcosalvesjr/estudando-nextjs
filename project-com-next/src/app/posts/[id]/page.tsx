eea43013723c2e79cb0ac178eda1171227a76464
Marcos Alves <mrcaurelio95@gmail.com>
2025-05-18 19:17:05 -0300

    loading ui e streaming

--- project-com-next/src/app/posts/[id]/page.tsx
+++ project-com-next/src/app/posts/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+import { PostInfo } from "../_components/post";
 import { PostProps } from "../page";
 
 export default async function DetailsPost({
@@ -7,13 +9,12 @@ export default async function DetailsPost({
 }) {
   const { id } = await params;
 
-  const response = await fetch(`https://dummyjson.com/posts/${id}`);
-  const data: PostProps = await response.json();
   return (
     <div>
-      <h1>Detalhes do post: {id}</h1>
-      <h2>{data.title}</h2>
-      <p>{data.body}</p>
+      <h1 className="text-center font-bold text-4xl">Detalhes do post: {id}</h1>
+      <Suspense fallback={<h1>Carregando...</h1>}>
+        <PostInfo id={id} />
+      </Suspense>
     </div>
   );
 }
